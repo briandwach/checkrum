abecdca0db9d393e4e4fd3e73ca0687d5869ddfc
Lotte-Et <charlotte.stowe@gmail.com>
2024-05-19 11:09:59 -0400

    Started AddLocationForm; Implemented hide/show logic on new client form

---
+++ client/src/components/AddLocationForm/index.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import Auth from '../../utils/auth';
+
+const AddLocationForm = ({clientId}) => {
+
+    const [ locationName, setLocationName ] = useState('');
+    const [ address, setLocationAddress ] = useState('');
+    const [ accessInstructions, setAccessInstructions ] = useState('');
+
+    return (
+        <div>
+        <h2> Location Information</h2>
+            <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text">Locations</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+            </label>
+            <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text">Location Address</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+            </label>
+            <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text">Access Instructions</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+            </label>
+            <label className="form-control w-full max-w-xs">
+                <div className="label">
+                    <span className="label-text">Room Name</span>
+                </div>
+                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+            </label>
+
+        </div>
+    )
+
+}
+
+export default AddLocationForm;
