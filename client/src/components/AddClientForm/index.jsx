33def84c4fe79c3771622c13b68a08263565777e
Lotte-Et <charlotte.stowe@gmail.com>
2024-05-24 23:21:12 -0400

    Added button and form for new location

--- client/src/components/AddClientForm/index.jsx
+++ client/src/components/AddClientForm/index.jsx
@@ -16,18 +16,31 @@ const AddClientForm = () => {
     const [ showLocationBtn, setShowLocationBtn ] = useState(false);
     const [ newClient, setNewClient ] = useState(true);
     const [ clientName, setClientName ] = useState('');
+    const [ addLocationButton, setAddLocationButton ] = useState( false );
 
-      const showLocationForm = async (event) => {
-        await setShowLocationBtn(true);
-        console.log(showLocationBtn);
+    //TO DO
+    //Add button to add new client after form is completed
+
+    
+    const showLocationForm = async (event) => {
+      await setShowLocationBtn(true);
+      console.log(showLocationBtn);
       };
+
+      const handleAddLocation = async (event) => {
+        await setAddLocationButton( true );
+      }
 
       return (
         <div>
 
           { newClient === true ? <NewClientForm setNewClient={setNewClient} newClient={newClient} setClientName={setClientName}/> : null}
            { newClient === false? <ClientCard clientName={clientName} /> : null }
+           { newClient === false?  <button type="button" className="btn" onClick={()=>handleAddLocation()}>Add a Location heere</button>: null}
+           { addLocationButton === true? <AddLocationForm />: null}
            {console.log(clientName)}
+           {console.log(addLocationButton)}
+
         </div>
       )
 }
