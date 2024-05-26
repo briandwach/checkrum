fdf64447d93ab1892519ee1cfcf0c4b77952cb9a
drewhermanson <97925194+drewhermanson@users.noreply.github.com>
2024-05-26 14:57:31 -0500

    Changed header to a nav bar that has a side panel on mobile

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -14,38 +14,84 @@ const Header = () => {
     htmlTag.dataset.theme = 'business';
   }, []);
 
+
   return (
-    <header className="bg-primary text-light mb-4 py-3 flex flex-wrap justify-center items-center">
-      <div className="container flex flex-wrap justify-between lg:justify-center items-center mx-auto">
-        <div>
-          <Link className="text-light" to="/">
-            <h1 className="justify-center text-white text-2xl font-bold">Checkrūm: Inspection Tracker</h1>
-          </Link>
+    <header>
+          <div className="drawer">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
+      <div className="drawer-content flex flex-col">
+        {/* Navbar */}
+        <div className="w-full navbar bg-base-300">
+          <div className="flex-none lg:hidden">
+            <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+            </label>
+          </div> 
+          <div className="flex-1 px-2 mx-2">Checkrum: Inspection Tracker</div>
+          <div className="flex-none hidden lg:block">
+            <ul className="menu menu-horizontal">
+              {Auth.loggedIn() ? (
+                <>
+                <button className="btn btn-sm btn-info m-2" onClick={logout}>
+                  Logout
+                </button>
+                </>
+                ) : (
+                  <>
+                    <li><Link className="btn btn-sm btn-info m-2" to="/login">
+                      Logout
+                    </Link></li>
+                    <li><Link className="btn btn-sm btn-info m-2" to="/signup">
+                      Signup
+                    </Link></li>
+                  </>
+                )}
+
+              <li>
+              <label className="flex cursor-pointer gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                <input type="checkbox" value="corporate" className="toggle theme-controller"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+              </label>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div>
-          {Auth.loggedIn() ? (
-            <>
-              <Link className="btn btn-lg btn-info m-2" to="/me">
-                {/* Run the getProfile() method to get access to the unencrypted token value in order to retrieve the user's username  */}
-                {Auth.getProfile().authenticatedPerson.username}'s profile
-              </Link>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link className="btn btn-lg btn-info m-2" to="/login">
-                Login
-              </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
-                Signup
-              </Link>
-            </>
-          )}
-        </div>
+        
+
+      </div> 
+      <div className="drawer-side">
+        <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label> 
+        <ul className="menu p-4 w-80 min-h-full bg-base-200">
+        {Auth.loggedIn() ? (
+                <>
+                <button className="btn btn-sm btn-info m-2" onClick={logout}>
+                  Logout
+                </button>
+                </>
+                ) : (
+                  <>
+                    <li><Link className="btn btn-sm btn-info m-2" to="/login">
+                      Logout
+                    </Link></li>
+                    <li><Link className="btn btn-sm btn-info m-2" to="/signup">
+                      Signup
+                    </Link></li>
+                  </>
+                )}
+
+              <li>
+              <label className="flex cursor-pointer gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                <input type="checkbox" value="corporate" className="toggle theme-controller"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+              </label>
+              </li>
+        </ul>
       </div>
+    </div>
     </header>
+ 
   );
 };
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,34 @@
-import { useQuery } from '@apollo/client';
-
 import { Link } from 'react-router-dom';
+
 
 const Home = () => {
 
-  return (
-    <main>
-      <Link to='/Staff'>Staff Page</Link><br />
-      <Link to='/Manager'>Manager Page</Link><br />
-      <Link to='/Admin'>Data Admin Page</Link><br />
-      <Link to='/Equipment'>Equipment Admin Page</Link><br />
-      <Link to='/Rooms'>All Rooms Page</Link>
-    </main>
-  );
+    return (
+      <>
+        <main style={{ display: 'flex', justifyContent: 'center', height: '100vh' }}>
+          <div style={{ textAlign: 'center' }}>
+            <h1>Welcome to our Homepage</h1>
+            <p>Discover amazing things with our awesome website!</p>
+           
+          </div>
+        </main>
+      </>
+    );
 };
 
+
+  
+
 export default Home;
+
+// return (
+//   <main>
+//   <Link to='/Staff'>Staff Page</Link><br />
+//   <Link to='/Manager'>Manager Page</Link><br />
+ 
+//   <Link to='/Admin'>Data Admin Page</Link><br />
+//   <Link to='/Equipment'>Equipment Admin Page</Link><br />
+//   <Link to='/Rooms'>All Rooms Page</Link>
+// </main>
+// );
+// };
