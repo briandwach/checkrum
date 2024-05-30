95357148b72b595561e950c5a468ca1dccf47466
drewhermanson <97925194+drewhermanson@users.noreply.github.com>
2024-05-29 20:42:12 -0500

    added go back button when choosing to inspect a room, add refetch to inspections to keep the data current

--- client/src/components/AssignedReportCard/index.jsx
+++ client/src/components/AssignedReportCard/index.jsx
@@ -6,7 +6,7 @@ function AssignedReportCard({ id, name, client, location, address, cycle, lastIn
     const { upcomingDueDate, timeToUpcomingDueDate, overdueStatus, missedCycles, initialMissedDate } = dateTimeProperties;
    
     return (
-        <div className="card lg:card-side bg-base-100 bg-slate-300 shadow-xl m-5">
+        <div className="card bg-secondary shadow-xl m-5">
             <div className="card-body">
                 <h2 className="card-title">Room: {name}</h2>
                 <p><span className="font-bold">Client: </span>{client}</p>
--- client/src/components/StaffDashboard/AssignedReports.jsx
+++ client/src/components/StaffDashboard/AssignedReports.jsx
@@ -1,48 +1,56 @@
 import AssignedReportCard from '../AssignedReportCard';
+import React, { useEffect } from 'react';
+
 
 import { useQuery } from '@apollo/client';
 import { ASSIGNED_REPORTS_BY_STAFF } from '../../utils/queries';
 
 const AssignedReports = ({ assignedStaff }) => {
+    const { loading, data, refetch } = useQuery(ASSIGNED_REPORTS_BY_STAFF, {
+        variables: { assignedStaff },
+        // Set the `fetchPolicy` to "network-only" to always fetch the latest data. AKA don't use the cache
+        fetchPolicy: 'network-only',
+    });
 
-const { loading, data } = useQuery(ASSIGNED_REPORTS_BY_STAFF, {
-    variables: { assignedStaff }
-});
+    // Call the `refetch` function whenever the component loads
+    useEffect(() => {
+        refetch();
+    }, []);
 
-if (loading) {
-    return <div>Loading...</div>;
-  }
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
-  const { assignedReportsByStaff } = data;
+    const { assignedReportsByStaff } = data;
 
-  return (
-      <div>
-          <h1 className="ml-3 text-3xl font-bold text-center">Assigned Inspections</h1>
+    return (
+        <div>
+            <h1 className="ml-3 text-3xl font-bold text-center">Assigned Inspections</h1>
 
             {assignedReportsByStaff.length < 1 ? (
                 <div>
-                <p  className="m-3">You currently have no assigned inspections.</p>
+                    <p className="m-3">You currently have no assigned inspections.</p>
                 </div>
             ) : (
-          <div className="">
-              {assignedReportsByStaff.map((report) => (
-                  <AssignedReportCard
-                      key={report._id}
-                      id={report._id}
-                      name={report.roomId.roomName}
-                      client={report.roomId.location.client.businessName}
-                      location={report.roomId.location.locationName}
-                      address={report.roomId.location.address}
-                      cycle={report.roomId.inspectionCycleLength}
-                      lastInspected={report.roomId.lastInspectionDate}
-                      dateTimeProperties={report.roomId.dateTimeProperties}
-                      completed={false}
-                  />
-              ))}
-          </div>
+                <div className="">
+                    {assignedReportsByStaff.map((report) => (
+                        <AssignedReportCard
+                            key={report._id}
+                            id={report._id}
+                            name={report.roomId.roomName}
+                            client={report.roomId.location.client.businessName}
+                            location={report.roomId.location.locationName}
+                            address={report.roomId.location.address}
+                            cycle={report.roomId.inspectionCycleLength}
+                            lastInspected={report.roomId.lastInspectionDate}
+                            dateTimeProperties={report.roomId.dateTimeProperties}
+                            completed={false}
+                        />
+                    ))}
+                </div>
             )}
-      </div>
-  );
-}
+        </div>
+    );
+};
 
 export default AssignedReports;
