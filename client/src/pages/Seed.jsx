ea318ee1e62de9f508998fc122033cb09cf36a74
livefromthelevel <brian.d.wach@gmail.com>
2024-06-02 20:05:25 -0500

    6.2/SeedingForDemo/Moved seed buttons to a seed page

--- client/src/pages/Seed.jsx
+++ client/src/pages/Seed.jsx
@@ -1,29 +1,9 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { SEED } from '../utils/mutations';
+import DbButtons from '../components/DbButtons';
 
 const Seed = () => {
-    const [successMessage, setSuccessMessage] = useState('');
-    const [seedDatabase] = useMutation(SEED);
-
-    const seedButton = async () => {
-        try {
-            await seedDatabase();
-            setSuccessMessage('Database reset and seeded successfully!');
-        } catch (error) {
-            console.error(error);
-            setSuccessMessage('Error seeding database. Check console for errors!');
-        }
-    };
-
     return (
-        <div>
-            <button className="btn btn-outline btn-accent" onClick={seedButton}>
-                Reset and Seed Database
-            </button>
-            {successMessage && <p>{successMessage}</p>}
-            <Link to="/">Return to homepage</Link>
+        <div className='flex justify-center m-6'>
+        <DbButtons />
         </div>
     );
 };
